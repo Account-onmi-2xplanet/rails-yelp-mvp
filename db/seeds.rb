c7e08ed1e7228225ff30e5c8458929270676f90e
Bruno Parente <parente.bruno@gmail.com>
2020-09-17 15:11:33 -0300

    yelp mvp rails solved

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE1",  phone_number: "(21)22221", category: "chinese" }
+dishoom2 = { name: "Dishoom2", address: "7 Boundary St, London E2 7JE2",  phone_number: "(21)22222", category: "italian" }
+dishuum3 = { name: "Dishuum3", address: "7 Boundary St, London E2 7JE3",  phone_number: "(21)222223", category: "japanese" }
+dishoom4 = { name: "Dishoom4", address: "7 Boundary St, London E2 7JE4",  phone_number: "(21)222224", category: "french" }
+dishoom5 = { name: "Dishoom5", address: "7 Boundary St, London E2 7JE5",  phone_number: "(21)222225", category: "belgian" }
+
+[ dishoom, dishoom2, dishuum3, dishoom4, dishoom5 ].each do |attributes|
+    restaurant = Restaurant.create!(attributes)
+    puts "Created #{restaurant.name}"
+  end
+  puts "Finished!"
